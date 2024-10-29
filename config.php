37644ac60cc28089ac54a7ab3778351d4ad9e8ab
Zorglube <630192+zorglube@users.noreply.github.com>
2024-10-29 14:35:37 +0100

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $dbport = getenv('POSTGRESQL_ADDON_PORT'); // PHPBB_DB_PORT="${POSTGRESQL_ADDON_
 $dbname = getenv('POSTGRESQL_ADDON_DB'); // PHPBB_DB_NAME="${POSTGRESQL_ADDON_DB}"
 $dbuser = getenv('POSTGRESQL_ADDON_USER'); // PHPBB_DB_USER="${POSTGRESQL_ADDON_USER}"
 $dbpasswd = getenv('POSTGRESQL_ADDON_PASSWORD'); // PHPBB_DB_PASSWD="${POSTGRESQL_ADDON_PASSWORD}"
-$table_prefix = getenv('PHPBB_DB_TABLE_PREFIX'); // PHPBB_DB_TABLE_PREFIX="phpbb_"
+$table_prefix = getenv('PHPBB_DB_TABLE_PREFIX'); // PHPBB_DB_TABLE_PREFIX="..."
 $phpbb_adm_relative_path = 'adm/';
 $acm_type = 'phpbb\\cache\\driver\\file';
 
